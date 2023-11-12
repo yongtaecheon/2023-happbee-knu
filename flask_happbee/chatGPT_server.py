3843e73f997fa595c38fb88d7fc12e8e09bde5ab
EunjiKim02 <now0104@naver.com>
2023-11-12 21:54:46 +0900

    for merge

--- flask_happbee/chatGPT_server.py
+++ flask_happbee/chatGPT_server.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 # set api key
 client = OpenAI(
-  api_key="",  # this is also the default, it can be omitted
+  api_key="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",  # this is also the default, it can be omitted
 )
 
 chat_count = 0
