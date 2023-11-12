810dc8d52ce47b45efec253d2843538e952b71c4
뭄 <111948424+mchaewon@users.noreply.github.com>
2023-11-12 22:26:34 +0900

    Update chatGPT_server.py

--- flask_happbee/chatGPT_server.py
+++ flask_happbee/chatGPT_server.py
@@ -14,8 +14,7 @@ app = Flask(__name__)
 
 # set api key
 client = OpenAI(
-  # api_key=os.getenv('OPENAI_API_KEY'),  # this is also the default, it can be omitted
-  api_key="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"
+  api_key=os.getenv('OPENAI_API_KEY'),  # this is also the default, it can be omitted
 )
 
 chat_count = 0
