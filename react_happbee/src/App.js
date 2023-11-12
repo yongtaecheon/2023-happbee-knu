48cb2ad65f8c181cfe4dc73889b909e472dbe440
kimseyun314 <kimseyun314@naver.com>
2023-11-12 18:46:07 +0900

    Implementation SideBar Button UI

--- react_happbee/src/App.js
+++ react_happbee/src/App.js
@@ -13,13 +13,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 
-
-
 export default function App() {
 
   return (
     <div className="d-flex App bold">
-      <Nav/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Nav />} />
+            <Route path="survey/:surveyID" element={<Nav />} />
+            <Route path="/main" element={<Nav />} />
+            <Route path="/chat" element={<Nav />} />
+            <Route path="/item" element={<Nav />} />
+            <Route path="/hosp" element={<Nav />} />
+            <Route path="/result" element={<Nav />} />
+        </Routes>
+      </BrowserRouter>
       <div className="Content bolder">
         <BrowserRouter>
           <Routes>
--- react_happbee/src/components/Nav.js
+++ react_happbee/src/components/Nav.js
@@ -1,41 +1,45 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useLocation } from 'react-router-dom';
+import "../sidebars.css";
 
 export default function Nav() {
+  const location = useLocation();
+  const pathname = location.pathname;
+  
   return (
       <div className="Nav d-flex flex-column flex-shrink-0 p-3 bg-light">
         <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-        <span className="bolder fs-4">햅비 HAPPBEE</span>
+          <span className="bolder fs-4">햅비 HAPPBEE</span>
         </a>
-        <hr/>
-      <ul className="nav nav-pills flex-column mb-auto">
+        <hr />
+        <ul className="nav nav-pills flex-column mb-auto">
           <li className="nav-item mt-5 mb-5">
-            <a href="/main" className="nav-link active link" aria-current="page">
+            <a href="/main" className={`nav-link ${pathname === '/main' ? "active-link" : "link-dark"}`} aria-current="page">
               <i class="bi bi-house-door"></i>
               <br></br>홈
             </a>
           </li>
           <li className="nav-item mb-5">
-            <a href="/chat" className="nav-link link-dark">
+            <a href="/chat" className={`nav-link ${pathname === '/chat' ? "active-link" : "link-dark"}`}>
               <i class="bi bi-chat-dots"></i>
               <br></br>채팅
             </a>
           </li>
           <li className="nav-item mb-5">
-            <a href="/survey/0" className="nav-link link-dark">
+            <a href="/survey/0" className={`nav-link ${pathname === '/survey/0' ? "active-link" : "link-dark"}`}>
               <i class="bi bi-clipboard2-heart"></i>
               <br></br>햅비지수
             </a>
           </li>
           <li className="nav-item mb-5">
-            <a href="/hosp" className="nav-link link-dark">
+            <a href="/hosp" className={`nav-link ${pathname === '/hosp' ? "active-link" : "link-dark"}`}>
               <i class="bi bi-heart-pulse"></i>
               <br></br>병원과 <br></br>상담하기
             </a>
           </li>
         </ul>
-        <hr/>
+        <hr />
       </div>
     );
-
 }
